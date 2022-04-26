d34297537066320df4554e38c07173b014782bc4
Eshan Chatterjee <eshan2001221@gmail.com>
2022-04-26 21:16:55 +0530

    fixed bugs in main

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ void printHelp(bool exit = true)
 }
 
 
-void initProject(std::filesystem::path currentPath)
+void initProject(const std::filesystem::path& currentPath)
 {
     // initialize the git repo
     plusutil::initGitRepository(currentPath.string());
@@ -38,53 +38,86 @@ void initProject(std::filesystem::path currentPath)
     plusutil::initGitFiles(currentPath.string());
 
     // create the src folder
-    plusutil::create_directory(currentPath().string() + "/src");
+    plusutil::create_directory(currentPath.string() + "/src");
 
     // create the main.cpp file
-    plusutil::create_file(currentPath().string() + "/src/main.cpp");
+    plusutil::create_file(currentPath.string() + "/src/main.cpp");
 
     // create the out directory
-    plusutil::create_directory(currentPath().string() + "/out");
+    plusutil::create_directory(currentPath.string() + "/out");
 
-    plusutil::createCmakeLists(std::filesystem::current_path().string(), std::filesystem::current_path().stem().string());
+    plusutil::createCmakeLists(currentPath.string(), currentPath.stem().string());
+    //plusutil::createCmakeLists(std::filesystem::current_path().string(), std::filesystem::current_path().stem().string());
 }
+
+[[maybe_unused]] void initProject(const std::filesystem::path& currentPath, const std::string& projectName)
+{
+    // initialize the git repo
+    plusutil::initGitRepository(currentPath.string());
+
+    // create the .gitignore and .gitattributes files
+    plusutil::initGitFiles(currentPath.string());
+
+    // create the src folder
+    plusutil::create_directory(currentPath.string() + "/src");
+
+    // create the main.cpp file
+    plusutil::create_file(currentPath.string() + "/src/main.cpp");
+
+    // create the out directory
+    plusutil::create_directory(currentPath.string() + "/out");
+
+    plusutil::createCmakeLists(currentPath.string(), projectName);
+    //plusutil::createCmakeLists(std::filesystem::current_path().string(), projectName);
+}
+
 
 int main(int argc, char** argv)
 {
-    std::vector<std::string> args(argv, argv + argc);
-
-    if (args.size() < 2)
+    if (argc < 2)
     {
         printHelp();
         return 1;
     }
 
-    else if(std::find(args.begin(), args.end(), "-h") != args.end()||
+    std::vector<std::string> args(argv, argv + argc);
+    std::filesystem::path currentPath = std::filesystem::current_path();
+
+    // plus -h or plus --help
+    if(std::find(args.begin(), args.end(), "-h") != args.end()||
         std::find(args.begin(), args.end(), "--help") != args.end())
     {
         printHelp(false);
         return 0;
     }
 
+    // plus -v or plus --version
     else if(std::find(args.begin(), args.end(), "-v") != args.end()||
         std::find(args.begin(), args.end(), "--version") != args.end())
     {
         fmt::print("plus v0.1\n");
+        printHelp();
         return 0;
     }
 
+    // plus init
     else if(std::find(args.begin(), args.end(), "init") != args.end())
     {
         fmt::print(fmt::emphasis::bold | fg(fmt::color::azure), "Initializing a new project\n");
-        initProject();
+        initProject(currentPath);
         return 0;
     }
 
+    // plus new "Hello World"
     else if(plusutil::get_project_name(args) != args.end())
     {
+        std::string projectName = plusutil::get_project_name(args);
+        std::string projectPath = currentPath.string() + "/" + projectName;
+
         fmt::print(fmt::emphasis::bold | fg(fmt::color::azure), "Creating a new project\n");
-        plusutil::create_directory(std::filesystem::current_path().string() + "/" + plusutil::get_project_name(args));
-        initProject();
+
+        plusutil::create_directory(projectPath);
+        initProject(projectPath);
         return 0;
     }
 
