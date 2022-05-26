90e6140be8803fbd2b9a20d67a377a1dbefd5530
Eshan Chatterjee <eshan2001221@gmail.com>
2022-05-26 18:08:48 +0530

    Update Script files

--- scripts/init.bat
+++ scripts/init.bat
@@ -1,1 +1,1 @@
-cmake -DCMAKE_TOOLCHAIN_FILE="D:/Program Files/vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Debug -S.. -B .
+cmake -DCMAKE_TOOLCHAIN_FILE="D:/Program Files/vcpkg/scripts/buildsystems/vcpkg.cmake" -S.. -B .
